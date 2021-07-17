f7318e2ac38bbe002caede59238cdb219d761484
Dragon-Seeker <50533513+Dragon-Seeker@users.noreply.github.com>
2021-07-16 23:08:02 -0500

    Attempt #1 to get the Bamboo Item Frames working
    It worked but more issues are still prevalent like missing textures/model and issues placing it

--- src/main/java/com/dragonseeker/tropicfabricport/entity/BambooItemFrameEntity.java
+++ src/main/java/com/dragonseeker/tropicfabricport/entity/BambooItemFrameEntity.java
@@ -1,8 +1,13 @@
 package com.dragonseeker.tropicfabricport.entity;
 
+import com.dragonseeker.tropicfabricport.Tropicfabricport;
 import com.dragonseeker.tropicfabricport.mixins.ItemFrameEntityInvoker;
 import com.dragonseeker.tropicfabricport.registry.TropicEntities;
 import com.dragonseeker.tropicfabricport.registry.TropicItems;
+import io.netty.buffer.Unpooled;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.network.ServerSidePacketRegistry;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ItemEntity;
@@ -11,13 +16,19 @@ import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.network.Packet;
+import net.minecraft.network.PacketByteBuf;
 import net.minecraft.network.packet.s2c.play.EntitySpawnS2CPacket;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.UUID;
+
 public class BambooItemFrameEntity extends ItemFrameEntity implements ItemFrameEntityInvoker {
+
+	public static Identifier SPAWN_PACKET = new Identifier(Tropicfabricport.MOD_ID, "bamboo_item_frame");
 
 	public BambooItemFrameEntity(World worldIn, BlockPos pos, Direction direction) {
 		this(TropicEntities.BAMBOO_ITEM_FRAME, worldIn, pos, direction);
@@ -33,6 +44,18 @@ public class BambooItemFrameEntity extends ItemFrameEntity implements ItemFrameE
 	public BambooItemFrameEntity(EntityType<BambooItemFrameEntity> bambooItemFrameEntityEntityType, World world) {
 		super(bambooItemFrameEntityEntityType, world);
 	}
+
+	@Environment(EnvType.CLIENT)
+	public BambooItemFrameEntity(World world, double x, double y, double z, Direction facing, int id, UUID uuid) {
+		super(TropicEntities.BAMBOO_ITEM_FRAME, world);
+		updatePosition(x, y, z);
+		updateTrackedPosition(x, y, z);
+		setEntityId(id);
+		setUuid(uuid);
+		setFacing(facing);
+	}
+
+
 
 	/*
 	@Override
@@ -69,7 +92,23 @@ public class BambooItemFrameEntity extends ItemFrameEntity implements ItemFrameE
 
 	@Override
 	public Packet<?> createSpawnPacket() {
-		return new EntitySpawnS2CPacket(this, this.getType(), this.facing.getId(), this.getDecorationBlockPos());
+		//return new EntitySpawnS2CPacket(this, this.getType(), this.facing.getId(), this.getDecorationBlockPos());
+
+		PacketByteBuf packet = new PacketByteBuf(Unpooled.buffer());
+
+		// entity position
+		packet.writeDouble(getX());
+		packet.writeDouble(getY());
+		packet.writeDouble(getZ());
+
+		// entity id & uuid
+		packet.writeInt(getEntityId());
+		packet.writeUuid(getUuid());
+
+		packet.writeInt(getHorizontalFacing().getId());
+
+		return ServerSidePacketRegistry.INSTANCE.toPacket(SPAWN_PACKET, packet);
+
     }
 
 	/*
--- src/main/java/com/dragonseeker/tropicfabricport/registry/TropicClientPackets.java
+++ src/main/java/com/dragonseeker/tropicfabricport/registry/TropicClientPackets.java
@@ -1,11 +1,18 @@
 package com.dragonseeker.tropicfabricport.registry;
 
 import com.dragonseeker.tropicfabricport.entity.AshenMaskEntity;
+import com.dragonseeker.tropicfabricport.entity.BambooItemFrameEntity;
 import com.dragonseeker.tropicfabricport.entity.ExplodingCoconutEntity;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.network.ClientSidePacketRegistry;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.decoration.ItemFrameEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.registry.Registry;
 
 import java.util.UUID;
 
@@ -40,5 +47,34 @@ public class TropicClientPackets {
                 MinecraftClient.getInstance().world.addEntity(entityID, explCoconut);
             });
         });
+
+
+
+        ClientSidePacketRegistry.INSTANCE.register(BambooItemFrameEntity.SPAWN_PACKET, (context, packet) -> {
+            double x = packet.readDouble();
+            double y = packet.readDouble();
+            double z = packet.readDouble();
+
+            int entityID = packet.readInt();
+            UUID entityUUID = packet.readUuid();
+
+            //else if (entityType == EntityType.ITEM_FRAME) {
+            //    entity15 = new ItemFrameEntity(this.world, new BlockPos(d, e, f), Direction.byId(packet.getEntityData()));
+
+            //pitch = packet.readByte();
+            //yaw = packet.readByte();
+            //this.entityData = packet.readInt();
+            //velocityX = packet.readShort();
+            //velocityY = packet.readShort();
+            //velocityZ = packet.readShort();
+
+            Direction direction = Direction.byId(packet.readInt());
+            context.getTaskQueue().execute(() -> {
+                //BambooItemFrameEntity bambooFrame = new BambooItemFrameEntity(MinecraftClient.getInstance().world, x, y, z, entityID, entityUUID);
+                BambooItemFrameEntity bambooFrame = new BambooItemFrameEntity(MinecraftClient.getInstance().world, x, y, z, direction, entityID, entityUUID);;
+                MinecraftClient.getInstance().world.addEntity(entityID, bambooFrame);
+            });
+        });
+
     }
 }
