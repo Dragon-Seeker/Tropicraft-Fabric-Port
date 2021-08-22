6a6fb46ea62aa81493941eef63eaf592e251b21f
Dragon-Seeker <50533513+Dragon-Seeker@users.noreply.github.com>
2021-08-21 19:48:11 -0500

    Add more Entity attributes for the future and removed forge calls
    Fixed issues with the jsons used to spawn entitys like the koa and the fish around the village

--- src/main/java/net/tropicraft/core/common/registry/TropicraftEntityAttributes.java
+++ src/main/java/net/tropicraft/core/common/registry/TropicraftEntityAttributes.java
@@ -7,10 +7,15 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class TropicraftEntityAttributes {
-    public static final EntityAttribute ENTITY_GRAVITY = new ClampedEntityAttribute("forge.entity_gravity", 0.08D, -8.0D, 8.0D).setTracked(true);
+    public static final EntityAttribute ENTITY_GRAVITY = new ClampedEntityAttribute("tropicraft.entity_gravity", 0.08D, -8.0D, 8.0D).setTracked(true);
+    public static final EntityAttribute SWIM_SPEED = new ClampedEntityAttribute("tropicraft.swimSpeed", 1.0D, 0.0D, 1024.0D).setTracked(true);
+    public static final EntityAttribute NAMETAG_DISTANCE = new ClampedEntityAttribute("tropicraft.nameTagDistance", 64.0D, 0.0D, 64.0).setTracked(true);
+
 
     public void init(){
         register("entity_gravity", ENTITY_GRAVITY);
+        register("swim_speed", SWIM_SPEED);
+        register("nametag_distance", NAMETAG_DISTANCE);
     }
 
     private static EntityAttribute register(String id, EntityAttribute attribute) {
